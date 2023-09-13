f5f87141f9b18ad1e562967c395402f9c332b17d
Andrey Gudin <aeon150791@gmail.com>
2023-09-13 15:00:00 +0400

    feat: add for shopping list variant for no items in shopping list

--- src/entities/ShoppingList/ui/ShoppingList.tsx
+++ src/entities/ShoppingList/ui/ShoppingList.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
-import { memo } from "react";
+import { memo, useContext } from "react";
 import type { FC } from "react";
 
 import ShoppingImage from "p/shopping.svg";
+import { Label } from "@/shared/ui/Label";
+import { ShoppingListContext } from "../model/context/ShoppingListContext";
+import { Input } from "@/shared/ui/Input";
 
 interface ShoppingListProps {
   className?: string;
@@ -11,14 +14,23 @@ interface ShoppingListProps {
 export const ShoppingList: FC<ShoppingListProps> = memo(function ShoppingList({
   className = "",
 }: ShoppingListProps) {
-  return (
-    <div className={`${className} flex flex-col h-full`}>
+  const { shoppingList } = useContext(ShoppingListContext);
+
+  const noItems = (
+    <div className={`${className} flex flex-col h-full justify-center`}>
+      <Label className='mt-auto' type={"medium"} sort={"center"}>
+        No items
+      </Label>
       <Image
-        className='self-end'
+        className='mt-auto'
         src={ShoppingImage}
         alt='shopping'
         width={245}
       />
     </div>
   );
+
+  if (shoppingList.length === 0) return noItems;
+
+  return <Input />;
 });
--- src/shared/ui/Label/Label.tsx
+++ src/shared/ui/Label/Label.tsx
@@ -13,12 +13,19 @@ export const labelVariants = cva(
       type: {
         regular: ["text-base leading-5 text-black"],
         white: ["text-base text-white"],
+        medium: ["text-sm font-bold"],
         small: ["text-xs text-textSmall"],
         big: ["text-[26px] leading-[32.5px]"],
+      },
+      sort: {
+        start: ["text-start"],
+        center: ["text-center"],
+        end: ["text-end"],
       },
     },
     defaultVariants: {
       type: "regular",
+      sort: "start",
     },
   }
 );
@@ -27,10 +34,10 @@ const Label = forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
     VariantProps<typeof labelVariants>
->(({ className, type, ...props }, ref) => (
+>(({ className, type, sort, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
-    className={cn(labelVariants({ type }), className)}
+    className={cn(labelVariants({ type, sort }), className)}
     {...props}
   />
 ));
